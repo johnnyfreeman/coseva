170d97256279c5134cddf7f76ebd4f5cb34c90b4
Johnny Freeman <prsjohnny@gmail.com>
2012-09-04 10:43:27 -0400

    finished the toTable method and adjusted the firing order of the filters.

--- src/CSV.php
+++ src/CSV.php
@@ -25,7 +25,7 @@ class CSV
         }
     }
 
-    public function filterRow($callable)
+    public function filterRow($callable, $position = 'before')
     {
         if (is_callable($callable)) {
             $this->_rowFilters[] = $callable;
@@ -36,23 +36,23 @@ class CSV
     {
         foreach(new LimitIterator($this->_file, $rowOffset) as $row => $columns)
         {
-            // run row filters
-            foreach ($this->_rowFilters as $filter) {
-                $columns = call_user_func($filter, $columns);
-            }
-
-            foreach ($columns as $col => $value)
+            foreach ($columns as $col => &$value)
             {
                 // run column filters
                 if (isset($this->_columnFilters[$col]))
                 {
                     foreach ($this->_columnFilters[$col] as $filter) {
-                        $value = call_user_func($filter, $value);
+                        $value = call_user_func($filter, $value, $col);
                     }
                 }
-
-                $this->_rows[$row][$col] = $value;
             }
+
+            // run row filters
+            foreach ($this->_rowFilters as $filter) {
+                $columns = call_user_func($filter, $columns, $row);
+            }
+
+            $this->_rows[$row] = $columns;
         }
     }
 
@@ -63,121 +63,38 @@ class CSV
 
     public function toTable()
     {
-        // $num_rows = count($this->_rows);
+        $rows = $this->toArray();
+        $num_rows = count($rows);
 
-        // if (0 === $num_rows) {
-        //  return;
-        // }
+        // begin drawing table
+        $output = '<table border="1" cellspacing="1" cellpadding="1">';
 
-        // $num_cols = count($this->_rows[0]);
+        // thead
+        if (count($this->_rows)) {
+            $output .= '<thead><tr><th>&nbsp;</th>';
+            foreach ($rows as $row) {
+                foreach ($row as $key => $col) {
+                    $output .= '<th>' . $key .  '</th>';
+                }
+                break;
+            }
+            $output .= '</tr></thead>';
 
-        // // open table
-        // $output = '<table border="1">';
+            // tbody
+            $output .= '<tbody>';
+            foreach ($rows as $i => $row) {
+                $output .= '<tr>';
+                $output .= '<td>' . $i . '</td>';
+                foreach ($row as $col) {
+                     $output .= '<td>' . $col .  '</td>';
+                }
+                $output .= '</tr>';
+            }
+            $output .= '</tbody>';
+        }
+        
+        $output .= '</table>';
 
-        // // table header
-        // $output = '<thead><tr><th>#</th><th>0</th><th>1</th><th>2</th><th>3</th><th>4</th><th>5</th><th>6</th><th>7</th><th>8</th><th>9</th><th>10</th><th>11</th><th>12</th><th>13</th><th>14</th><th>15</th><th>16</th><th>17</th><th>18</th><th>19</th><th>20</th><th>21</th><th>22</th><th>23</th><th>24</th><th>25</th><th>26</th><th>27</th><th>28</th><th>29</th><th>30</th><th>31</th><th>32</th><th>33</th><th>34</th><th>35</th><th>36</th><th>37</th><th>38</th><th>39</th><th>40</th><th>41</th><th>42</th><th>43</th><th>44</th><th>45</th></tr></thead>';
-        // $output = '<tbody>';
-        // foreach ($this->_rows as $row) {
-            
-        // }
-
-     //    while (($columns = fgetcsv($handle, 1000, ",")) !== FALSE)
-     //    {
-     //        $num_col = count($columns);
-
-     //        // echo '<tr>';
-     //        // echo '<td>' . $row . '</td>';
-
-     //        // for ($i=0; $i < $num_col; $i++)
-     //        // {
-     //        //     echo '<td>' . $columns[$i] . '</td>';
-     //        // }
-     //        // echo '</tr>';
-
-     //        $code                = $columns[0];
-     //        $mod1                = $columns[1];
-     //        $mod2                = $columns[2];
-     //        $description         = $columns[3];
-     //        $allowable_non_fac   = toNum($columns[41]);
-     //        $allowable_fac       = toNum($columns[42]);
-
-     //        // use php's binary calculater for accuracy
-     //        $twenty_non_fac      = bcmul($allowable_non_fac, '.2');
-     //        $twenty_fac          = bcmul($allowable_fac, '.2');
-     //        $self_pay_non_fac    = bcmul($allowable_non_fac, '1.2');
-     //        $self_pay_fac        = bcmul($allowable_fac, '1.2');
-
-     //        $work_rvu            = toNum($columns[6]);
-     //        $global_days     = (int) toNum($columns[24]);
-     //        $asst_surgeon        = toNum($columns[30]);
-     //        $dosage              = NULL;
-     //        $qty             = serialize(array('1', '2'));
-
-     //        // prepare
-     //        $code                = prepare($code);
-     //        $mod1                = prepare($mod1);
-     //        $mod2                = prepare($mod2);
-     //        $description         = prepare($description);
-     //        $allowable_non_fac   = prepare($allowable_non_fac);
-     //        $allowable_fac       = prepare($allowable_fac);
-     //        $twenty_non_fac      = prepare($twenty_non_fac);
-     //        $twenty_fac          = prepare($twenty_fac);
-     //        $self_pay_non_fac    = prepare($self_pay_non_fac);
-     //        $self_pay_fac        = prepare($self_pay_fac);
-     //        $work_rvu            = prepare($work_rvu);
-     //        $global_days     = prepare($global_days);
-     //        $asst_surgeon        = prepare($asst_surgeon);
-     //        $dosage              = prepare($dosage);
-     //        $qty             = prepare($qty);
-
-     //        $QRY = $DB->prepare("INSERT INTO [VOVN_Web].[dbo].[cpt_codes]
-     //           ([id]
-     //          ,[code]
-        //       ,[mod1]
-        //       ,[mod2]
-        //       ,[description]
-        //       ,[dosage]
-        //       ,[qty]
-        //       ,[allowable_non_fac]
-        //       ,[allowable_fac]
-        //       ,[twenty_non_fac]
-        //       ,[twenty_fac]
-        //       ,[self_pay_non_fac]
-        //       ,[self_pay_fac]
-        //       ,[work_rvu]
-        //       ,[global_days]
-        //       ,[asst_surgeon])
-     //     VALUES
-     //           ($row
-     //           ,$code
-     //           ,$mod1
-     //           ,$mod2
-     //           ,$description
-     //           ,$dosage
-     //           ,$qty
-     //           ,$allowable_non_fac
-     //           ,$allowable_fac
-     //           ,$twenty_non_fac
-     //           ,$twenty_fac
-     //           ,$self_pay_non_fac
-     //           ,$self_pay_fac
-     //           ,$work_rvu
-     //           ,$global_days
-     //           ,$asst_surgeon)");
-
-     //        // echo '<pre>'; print_r($QRY); echo '</pre>';
-
-     //        if (FALSE !== $QRY) {
-     //         $QRY->execute();
-     //     }
-
-     //        $row++;
-     //    }
-
-     //    echo '</tbody>';
-        // echo '</table>';
-
-
-        // return $output;
+        return $output;
     }
 }
