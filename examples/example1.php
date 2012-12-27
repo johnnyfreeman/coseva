5081fca2f0a2e63fc84c07a6946a532745487889
Johnny Freeman <prsjohnny@gmail.com>
2012-12-27 09:46:10 -0500

    changed the first example back to its simplest usage. will add a more exaggerated example later.

--- examples/example1.php
+++ examples/example1.php
@@ -6,17 +6,6 @@ require('../src/CSV.php');
 // read
 $csv = new Coseva\CSV('example1.csv');
 
-// trucate text in 1s column
-$csv->filter(function($row) {
-	unset($row[0]);
-	return $row;
-});
-
-// Capitalize first letter of 1st column
-$csv->filter(function($col) {
-    return $col . '-first column';
-}, 0);
-
 // parse
 $csv->parse();
 
